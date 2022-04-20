4d6c33c8561cb50be5affa815a55b678c9275942
Tetiana1192 <tanya_strigun@ukr.net>
2022-04-20 21:16:12 +0300

    task1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+//const numberofCategories = document.querySelectorAll('li.item').length;
+//const categories = document.querySelectorAll('.item');
+//console.log(`Number of categories: ${numberofCategories}`);
+
+
+categories.forEach((category) => {
+    console.log(`Category: ${category.querySelector('h2').textContent}`);
+    console.log(`Elements: ${category.querySelectorAll('li').length}`);
+
+});
---
+++ js/task-02.js
@@ -0,0 +1,12 @@
+//const ingredients = [
+  //'Potatoes',
+  //'Mushrooms',
+  //'Garlic',
+  //'Tomatos',
+  //'Herbs',
+  //'Condiments',
+//];
+
+const element = document.createElement('li');
+concole.log(element);
+
